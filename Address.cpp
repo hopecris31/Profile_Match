1b1a1072453c0fa8b73e538953705e464332eaa8
hopecris31 <hope.crisafi@gmail.com>
2022-12-20 19:03:41 -0500

    added py files

--- Address.cpp
+++ Address.cpp
@@ -2,15 +2,19 @@
 // Created by Hope Crisafi on 12/16/22.
 //
 
-//#include <Python.h>
+#include <Python/Python.h>
+#include "ProfileDistanceFunctions.h"
 #include "Address.h"
 
+using namespace std;
+
 Address::Address(string city, string state) :
-        city_(std::move(city)),state_(std::move(state)){
-}
+        city_(std::move(city)),state_(std::move(state)){}
+
 
 string Address::get_data() {
-return"";
+
+    return"";
 }
 
 
---
+++ ProfileDistanceFunctions.h
@@ -0,0 +1,14 @@
+//
+// Created by Hope Crisafi on 12/20/22.
+//
+
+#ifndef UNTITLED2_PROFILEDISTANCEFUNCTIONS_H
+#define UNTITLED2_PROFILEDISTANCEFUNCTIONS_H
+
+#include <string>
+
+using namespace std;
+
+string get_city_data(string city, string state);
+
+#endif //UNTITLED2_PROFILEDISTANCEFUNCTIONS_H
